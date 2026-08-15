aff046c5b67ac7c7f28ba462d1e87f7c4a428181
Ankit Raj <ankitRaj0427@outlook.com>
2026-08-15 22:58:15 +0530

    Time: 1544 ms (9.80%) | Memory: 21 MB (43.37%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,5 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        res=[]
+        for num in nums:
+            if nums.count(num)==1: res.append(num)
+            if len(res)==2: break
+        return res
+
+        '''
         xall=0
         for num in nums:
             xall ^= num
@@ -9,3 +16,5 @@ class Solution:
             if num & diff: a^=num
             else: b^=num
         return [a,b]
+        '''
+
